c9a59732d5df352681037ec9418bb2f60ac7e9b7
LeoPrince <meobius123@gmail.com>
2020-03-13 02:21:45 +0800

    Updates

---
+++ precache-manifest.54471fd9c3dd764725aeda2b7acd9d54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96793b4a2a1e25a25e4444ef8ab08f64",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "00d00a69dc5d58fc71c5",
+    "url": "/my-react-portfolio/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "f82250c61c50a2e03c65",
+    "url": "/my-react-portfolio/static/js/2.1c78273f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-react-portfolio/static/js/2.1c78273f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d00a69dc5d58fc71c5",
+    "url": "/my-react-portfolio/static/js/main.f0ed8a0a.chunk.js"
+  },
+  {
+    "revision": "5831fb7de6cc26cea9c6",
+    "url": "/my-react-portfolio/static/js/runtime-main.65fbdfe6.js"
+  }
+]);
