1c88dc1bc4d5133592398014b28d7f64a513a0cf
LeoPrince <meobius123@gmail.com>
2020-03-13 03:18:31 +0800

    update values

--- src/components/About.js
+++ src/components/About.js
@@ -8,26 +8,20 @@ export default class About extends Component {
             <div className="twelve columns main-col">
 
                <h2>About Me</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
+               <p>{ resumeData.aboutme }</p>
 
                <div className="row">
 
                   <div className="columns contact-details">
 
-                  <h2>Contact Details</h2>
-                  <p className="address">
-       						<span>{resumeData.name}</span>
-                     <br></br>
-       						   <span>
-                     {resumeData.address}
-                    </span>
-                    <br></br>
-                    <span>{resumeData.website}</span>
-       					   </p>
+                     <h2>Contact Details</h2>
+                     <p className="address">
+                        <span>{resumeData.name}</span>
+                        <br></br>
+                        <span>{ resumeData.address }</span>
+                        <br></br>
+                        <span>{resumeData.website}</span>
+                     </p>
                   </div>
                </div>
             </div>
