f29896e6719d68036d3630454a6a916a8e8eb3ce
LeoPrince <meobius123@gmail.com>
2020-03-13 04:06:47 +0800

    update css

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,6 @@ class Header extends React.Component {
                 <li><a className="smoothscroll" href="#about">About</a></li>
                 <li><a className="smoothscroll" href="#resume">Resume</a></li>
                 <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                 <li><a className="smoothscroll" href="#contact">Contact</a></li>
               </ul>
           </nav>
