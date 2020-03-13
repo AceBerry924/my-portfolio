a1403619c86e8217dc2ddb05e037d32b643a1098
LeoPrince <meobius123@gmail.com>
2020-03-13 15:02:18 +0800

    add private information

--- src/resumeData.js
+++ src/resumeData.js
@@ -20,6 +20,16 @@ let resumeData = {
           "name":"skype",
           "url":"skype:live:meobius123",
           "className":"fa fa-skype"
+        },
+        {
+          "name":"email",
+          "url":"mail:meobius123@gmail.com",
+          "className":"fa fa-envelope-o"
+        },
+        {
+          "name":"resume",
+          "url":"https://drive.google.com/open?id=1eBKmIC7z9v6chYkR8-Vt5zK5qsi95TbP",
+          "className":"fa fa-file-user"
         }
       ],
     "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
